b8a5de9a640f88a5a6e0684f8826171751980f3b
Nazareno Capria <nazarenocapria@gmail.com>
2021-03-14 18:00:21 -0300

    primer commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public GameObject sword;
+    public float rotationSpeed;
+    public bool canMove = true;
+    public bool isRotating;
+    public Vector3 rotateTo;
+    public float angle;
+    public Quaternion quatToRotate;
+    public Quaternion startRotation;
+    private Transform playerTransform;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerTransform = GetComponent<Transform>();
+        rotateTo = new Vector3();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (canMove)
+        {
+            angle = Mathf.Atan2(rotateTo.y - this.transform.position.y, rotateTo.x - this.transform.position.x) * Mathf.Rad2Deg;
+            Vector3 vector = new Vector3(0, 0, angle);
+
+            if(Vector3.Distance(transform.eulerAngles, rotateTo) < 0.01f)
+            {
+
+                    transform.eulerAngles = rotateTo;
+                    isRotating = false;
+                
+            }
+            if (Vector3.Distance(transform.eulerAngles, rotateTo) >= 0.01f)
+            {
+                isRotating = true;
+                
+                Rotate(startRotation, quatToRotate);
+            }
+            
+            if (Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Began && Input.touches[0].phase != TouchPhase.Moved && !isRotating)
+            {
+                checkAndMoveTouch();
+            }
+            else if (Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Moved)
+            {
+                checkAndMoveSlide();
+
+            }
+        }
+    }
+
+    public void checkAndMoveTouch()
+    {
+        
+            Vector3 touchPos = Input.GetTouch(0).position;
+            touchPos.z = -Camera.main.transform.position.z;
+            rotateTo = Camera.main.ScreenToWorldPoint(touchPos);
+            quatToRotate = Quaternion.Euler(transform.eulerAngles.x, transform.eulerAngles.y, angle);
+            startRotation = transform.rotation;
+    }
+    public void Rotate(Quaternion thisEuler, Quaternion to)
+    {
+        this.transform.rotation = Quaternion.Lerp(thisEuler, to, Time.deltaTime);
+    }
+    public void checkAndMoveSlide()
+    {
+            isRotating = false;
+
+            Vector3 touchPos = Input.GetTouch(0).position;
+            touchPos.z = -Camera.main.transform.position.z;
+            Vector3 rotateTo = Camera.main.ScreenToWorldPoint(touchPos);
+
+            Vector3 lookAtDir = rotateTo - this.transform.position;
+
+           
+            //Vector3 vector = new Vector3(playerTransform.rotation.x, playerTransform.rotation.y, angle);
+            this.transform.right = lookAtDir;
+        }
+
+
+    }
